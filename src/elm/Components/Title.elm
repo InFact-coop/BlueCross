bdc77a99d93f5b1b020bcffadbc6f7596ea9a7fd
Max Gerber <mgerber94@gmail.com>
2017-12-07 16:36:59 +0000

    header updates through model #9 #11

--- src/elm/Components/Title.elm
+++ src/elm/Components/Title.elm
@@ -7,4 +7,35 @@ import Types exposing (..)
 
 viewTitle : Model -> Html Msg
 viewTitle model =
-    header [ class "headerTitle bg-blue flex justify-center items-center white h4" ] [ text "Hey" ]
+    header [ class "headerTitle bg-blue flex justify-center items-center white h4" ] [ p [] [ text <| getTitle model.route ] ]
+
+
+getTitle : Route -> String
+getTitle route =
+    case route of
+        HomeRoute ->
+            "Welcome"
+
+        BeforeYouBeginRoute ->
+            "Before You Begin"
+
+        PetInfoRoute ->
+            "Pet Info"
+
+        LocationRoute ->
+            "Location"
+
+        PersonalityRoute ->
+            "Personality"
+
+        LikesRoute ->
+            "The Ideal Home"
+
+        OwnerInfoRoute ->
+            "Owner Info"
+
+        ThankYouRoute ->
+            ""
+
+        NotFoundRoute ->
+            "Sorry"
--- src/elm/Routes/HomePage.elm
+++ src/elm/Routes/HomePage.elm
@@ -4,13 +4,11 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Types exposing (..)
-import Components.Title exposing (..)
 
 
 home : Model -> Html Msg
 home model =
     div [ class "w-60-ns center" ]
-        [ viewTitle model
-        , p [ class "f3 w60 mh1 tc" ] [ text "Welcome to this SPA elm boiler plate. Tachyons is included. Go nuts." ]
+        [ p [ class "f3 w60 mh1 tc" ] [ text "Welcome to this SPA elm boiler plate. Tachyons is included. Go nuts." ]
         , input [ class "f3 w30 pa1 center db ba tc", onInput Change, value model.userInput, placeholder "I update state, yo" ] []
         ]
