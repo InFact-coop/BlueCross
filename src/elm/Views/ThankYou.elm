478a0bcabf1f840faa90ab5a76c616b9322995c6
jen-harris <jennifer.louise.harris@gmail.com>
2018-02-28 11:01:24 +0000

    start of thank you page #85

--- src/elm/Views/ThankYou.elm
+++ src/elm/Views/ThankYou.elm
@@ -1,27 +1,32 @@
 module Views.ThankYou exposing (..)
 
+import Helpers exposing (getPetName)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Types exposing (..)
-import Helpers exposing (getPetName)
 
 
 thankYou : Model -> Html Msg
 thankYou model =
     div [ class "" ]
-        [ div [ class "" ]
-            [ img [ src "./assets/doggo.png", class " w-100 db" ]
+        [ section [ class "dn-ns" ]
+            [ div [ class "" ]
+                [ img [ src "./assets/doggo.png", class " w-100 db" ]
+                    []
+                ]
+            , div
+                [ class "w-100 bg-blue white h-auto" ]
+                [ div [ class "tc pa2 pt3 b f3" ] [ text "Thank You!" ]
+                , div [ class "tc pl4 pr4 pb3 f5 fw2" ]
+                    [ div [ class "mb2" ] [ text <| "Thank you for filling out our questionnaire, this helps us match " ++ getPetName model ++ " with their new owner." ]
+                    , div [ class "b" ] [ text "We’ll be in touch within 72 hours." ]
+                    ]
+                ]
+            , div
+                [ class "headerTitle bg-blue flex justify-center items-center h2 mb4" ]
                 []
             ]
-        , div
-            [ class "w-100 bg-blue white h-auto" ]
-            [ div [ class "tc pa2 pt3 b f3" ] [ text "Thank You!" ]
-            , div [ class "tc pl4 pr4 pb3 f5 fw2" ]
-                [ div [ class "mb2" ] [ text <| "Thank you for filling out our questionnaire, this helps us match " ++ getPetName model ++ " with their new owner." ]
-                , div [ class "b" ] [ text "We’ll be in touch within 72 hours." ]
-                ]
+        , section [ class "dn dib-ns" ]
+            [ img [ src "./assets/doggo.png", class " w-100 db vh-90 contain" ] []
             ]
-        , div
-            [ class "headerTitle bg-blue flex justify-center items-center h2 mb4" ]
-            []
         ]
