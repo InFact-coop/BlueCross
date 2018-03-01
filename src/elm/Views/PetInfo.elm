e3e449d3752ddf715b28fdae64df476fbf23b177
jen-harris <jennifer.louise.harris@gmail.com>
2018-03-01 15:59:57 +0000

    adds upload image design relates #80

--- src/elm/Views/PetInfo.elm
+++ src/elm/Views/PetInfo.elm
@@ -19,7 +19,7 @@ onBlurValue tagger =
 
 petInfo : Model -> Html Msg
 petInfo model =
-    div [ class "w-60-ns center ma3 ma0-ns" ]
+    div [ class "w-60-ns w-90 center ma3 ma0-ns" ]
         [ div
             [ class " blue b mb2" ]
             [ text "What is your pet's name?" ]
@@ -43,7 +43,7 @@ petInfo model =
                 , p
                     [ class "mt0 blue" ]
                     [ text "&" ]
-                , select [ class "bg-light-blue bn w-80 w-50-ns gray tc pa3 mb3 f5 fw1 h2", id "dogBreed" ]
+                , select [ class "link w-25-ns w-100 bg-navy br2 white pa3 br2 f4 dib", id "dogBreed" ]
                     (List.map
                         dogBreedDropDown
                         dogBreedsList
--- src/elm/Views/Photos.elm
+++ src/elm/Views/Photos.elm
@@ -1,6 +1,7 @@
 module Views.Photos exposing (..)
 
 import Components.StyleHelpers exposing (classes, displayElement)
+import Helpers exposing (getPetName)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -14,30 +15,58 @@ photos model =
         imageList =
             List.map renderImage <| Maybe.withDefault [] model.image
     in
-        div [ class "ma3" ]
-            ([ label [ for model.imageId ] [ text "Upload your Own Photos" ]
-             , input
-                [ type_ "file"
-                , id model.imageId
-                , multiple True
-                , accept "image/*"
-                , on "change"
-                    (Decode.succeed ImageSelected)
+    div [ class "w-60-ns w-90 center ma3 " ]
+        ([ header [ class "b blue" ] [ text "So potential owners can see what Max looks like we would love to see a photo!" ]
+         , section []
+            [ p [ class "fw1" ]
+                [ img [ src "./assets/tick.png", class "mr2" ] []
+                , text <| "Please let us see " ++ getPetName model ++ "'s face"
                 ]
-                []
-             , div []
+            , p [ class "fw1" ]
+                [ img [ src "./assets/tick.png", class "mr2" ] []
+                , text <| "We only want to see " ++ getPetName model ++ " not " ++ getPetName model ++ "'s friends!"
+                ]
+            , p [ class "fw1" ]
+                [ img [ src "./assets/tick.png", class "mr2" ] []
+                , text "Only recent photos please"
+                ]
+            ]
+         , label [ for model.imageId, class "b blue" ] [ text <| ("Please take or upload a photo of " ++ getPetName model) ]
+         , br [ class "pa3" ] []
+         , div [ class "flex flex-wrap justify-center" ]
+            [ div [ class "ba w-40-ns w-80 vh-25 vh-50-ns br2 b--blue ma3 " ]
+                [ div [ class "h-100" ]
+                    [ div [ class " white f4 fw1  v-mid tc center h-100 w-100 upload-background " ]
+                        [ input
+                            [ class "h-100 bg-pink relative w-100 pointer bg-pink dib o-0"
+                            , type_ "file"
+                            , id model.imageId
+                            , multiple True
+                            , accept "image/*"
+                            , on "change"
+                                (Decode.succeed ImageSelected)
+                            ]
+                            []
+                        ]
+
+                    -- , div [ class "h-75 flex items-center" ] [ img [ src "./assets/upload.png", class " tc center flex v-mid w3 w4-ns" ] [ text "hello" ] ]
+                    ]
+                , div [ class "blue tc margin-minus dn db-ns" ] [ text "Upload Image" ]
+                ]
+            , div [ class "ba w-40-ns w-80 vh-25 vh-50-ns br2 b--blue ma3" ]
                 [ button [ class "mh2", onClick PreparePhoto ] [ text "1. Want to take a Photo?" ]
                 , button [ class "mh2", onClick TakePhoto ] [ text "2. Lights, Camera, Action!" ]
                 , button [ class "mh2", onClick StopPhoto ] [ text "3. I'm Done Taking Photos!" ]
                 , button [ class "mh2", onClick UploadPhotos ] [ text "4. Upload My Photos To The Cloud!" ]
                 ]
-             , Html.video [ classes [ displayElement <| model.liveVideoUrl /= "" ], autoplay True, src model.liveVideoUrl ] []
-             , div [ class "mt4 tc w-100" ]
-                [ a [ class "w-100 bg-navy br2 white pa3 br2 f4 dib no-underline", href "#location" ] [ text "Next" ]
+            , Html.video [ classes [ displayElement <| model.liveVideoUrl /= "" ], autoplay True, src model.liveVideoUrl ] []
+            , div [ class "mt4 tc w-100" ]
+                [ a [ class "link w-25-ns w-100 bg-navy br2 white pa3 br2 f4 dib", href "#finding-home" ] [ text "Next" ]
                 ]
-             ]
-                ++ imageList
-            )
+            ]
+         ]
+            ++ imageList
+        )
 
 
 renderImage : Image -> Html Msg
