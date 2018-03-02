fbe979ce43db6d1adf9a16574c10388144c7d8d4
jen-harris <jennifer.louise.harris@gmail.com>
2018-03-02 15:27:18 +0000

    adds a terms of service line at the start of app relates #108

--- src/elm/Views/Home.elm
+++ src/elm/Views/Home.elm
@@ -16,6 +16,8 @@ home model =
             ]
         , div [ class "w-100 center" ] [ h2 [ class "blue f5 pt4-ns" ] [ text "When do you need to rehome your pet?" ], div [ class "center" ] mapButtons ]
         , br [] []
+        , p [ class "tc gray fw1 mb1" ] [ text "By continuing you agree to our" ]
+        , p [ class "tc mt1" ] [ a [ href "https://www.bluecross.org.uk/terms-and-conditions", class "tc fw1 blue mt1 no-underline link", target "blank" ] [ text "terms of services" ] ]
         , a [ class <| "tc center flex justify-center w-100 w-25-l w-50-m br2 white pa3 br2 link f4 " ++ toggleNextClasses model, createNextLink model ] [ text "Next" ]
         ]
 
