ad2939e0855bb51906dac218395d976f0e3e6fe2
Max Gerber <mgerber94@gmail.com>
2018-03-06 13:54:50 +0000

    Fixed?

--- src/server/routers/api_router.js
+++ src/server/routers/api_router.js
@@ -46,16 +46,30 @@ router.route('/send-form').post((req, res, next) => {
   const internalEmailData = req.body;
   console.log('Internal Email Data: ', req.body);
   internalEmailData.name = 'Kelly';
-  internalEmailData.email = 'jennifer.louise.harris@gmail.com';
+  internalEmailData.email = process.env.INTERNAL_RECIPIENT;
   internalEmailData.subject = 'Someone is ready to rehome their dog!';
-
-  email('internal-confirmation', internalEmailData, (error, result) => {
-    if (error) {
-      console.log('Error: ', error);
+  const externalEmailData = {
+    email: internalEmailData.email,
+    subject: 'Blue Cross confirmation',
+    ownerName: internalEmailData.ownerName,
+    petName: internalEmailData.petName,
+    imageUrls: internalEmailData.imageUrls
+  };
+  email('internal-confirmation', internalEmailData, (intError, intResult) => {
+    if (intError) {
+      console.log('Internal Email Error: ', intError);
       return res.json({ success: false });
     }
-    console.log('Email Sent: ', result);
-    return res.json({ success: true });
+    console.log('Internal Email Sent: ', intResult);
+
+    email('external-confirmation', externalEmailData, (extError, extResult) => {
+      if (extError) {
+        console.log('External Email Error: ', extError);
+        return res.json({ success: false });
+      }
+      console.log('External Email Sent: ', extResult);
+      return res.json({ success: true });
+    });
   });
 });
 
